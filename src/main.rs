64c27c977c0db235d3d6f8b2bfdbe2d87ee22b51
Piotr Czajka <piotr.filip.czajka@gmail.com>
2024-12-01 15:06:38 +0100

    2024 day 1 done

--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,65 @@
 use std::collections::HashMap;
 
+type Error = Box<dyn std::error::Error>;
+
 fn main() {
-    let arg = std::env::args().nth(1).unwrap();
-    let data = std::fs::read_to_string(&arg).unwrap();
-    let function = match &arg[..] {
-        "11" => day1_part1,
-        "12" => day1_part2,
-        "21" => day2_part1,
-        "22" => day2_part2,
-        "31" => day3_part1,
-        "32" => day3_part2,
-        _ => unimplemented!()
+    let day: i32 = std::env::args()
+        .nth(1)
+        .expect("missing 1st argument - day")
+        .parse()
+        .unwrap();
+    let part: i32 = std::env::args()
+        .nth(2)
+        .expect("missing 2nd argument - part")
+        .parse()
+        .unwrap();
+    let input = std::fs::read_to_string(&String::from_iter([&day.to_string(), ".txt"]))
+        .expect(&format!("missing {day} input file"));
+
+    let function = match (day, part) {
+        (1, 1) => day1_part1,
+        (1, 2) => day1_part2,
+        _ => {
+            unimplemented!()
+        }
     };
-    let result = function(&data);
+
+    let result = function(&input).unwrap();
+
     println!("{result}");
 }
 
-fn day1_part1(data: &str) -> String {
-    let mut floor = 0;
-    for ch in data.chars() {
-        match ch {
-            ')' => {floor -= 1},
-            '(' => {floor += 1},
-            _ => {},
-        }
+fn day1_part1(input: &str) -> Result<i32, Error> {
+    let mut left_list: Vec<i32> = Vec::new();
+    let mut right_list: Vec<i32> = Vec::new();
+    for line in input.lines() {
+        let splitted: Result<Vec<i32>, _> = line.split_whitespace().map(|n| n.parse()).collect();
+        let splitted = splitted.expect("malformed list");
+        left_list.push(splitted[0]);
+        right_list.push(splitted[1]);
     }
-    floor.to_string()
+    left_list.sort();
+    right_list.sort();
+
+    Ok(std::iter::zip(left_list, right_list)
+        .map(|(left, right)| (left - right).abs())
+        .sum())
 }
 
-fn day1_part2(data: &str) -> String {
-    let mut floor = 0;
-    for (index, ch) in data.chars().enumerate() {
-        match ch {
-            ')' => {floor -= 1},
-            '(' => {floor += 1},
-            _ => {},
-        }
-        if floor == -1 {
-            return (index + 1).to_string();
-        }
-    }
-    floor.to_string()
-}
+fn day1_part2(input: &str) -> Result<i32, Error> {
+    let mut counter: HashMap<i32, i32> = HashMap::new();
 
-fn day2_part1(data: &str) -> String {
-    let mut total = 0;
-    for line in data.lines() {
-        let dupa = line.split("x").map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
-        let (l, w, h) = (dupa[0], dupa[1], dupa[2]);
-        let side1 = 2 * l * w;
-        let side2 = 2 * w * h;
-        let side3 = 2 * h * l;
-        total += side1 + side2 + side3 + vec![side1, side2, side3].iter().min().unwrap() / 2;
+    for right_num in input
+        .lines()
+        .filter_map(|line| line.split_whitespace().nth(1))
+        .filter_map(|number| number.parse::<i32>().ok())
+    {
+        *counter.entry(right_num).or_insert(0) += 1;
     }
-    return total.to_string()
-}
-
-fn day2_part2(data: &str) -> String {
-    let mut total = 0;
-    for line in data.lines() {
-        let mut dupa = line.split("x").map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
-        dupa.sort();
-        let (shortest, second_shortest, longest) = (dupa[0], dupa[1], dupa[2]);
-        let wrap_len = shortest * 2 + second_shortest * 2;
-        let bow_len = shortest * second_shortest * longest;
-        total += wrap_len + bow_len;
-    }
-    return total.to_string()
-}
-
-fn day3_part1(data: &str) -> String {
-    let mut house_presents: HashMap<(i32, i32), i32> = HashMap::new();
-    let mut position = (0, 0);
-    house_presents.insert(position, 1);
-    for ch in data.chars() {
-        match ch {
-            '<' => position.0 -= 1,
-            '>' => position.0 += 1,
-            '^' => position.1 += 1,
-            'v' => position.1 -= 1,
-            '\n' => {},
-            _ => {dbg!(ch); panic!("wrong input")},
-        }
-        *house_presents.entry(position).or_insert(-1) += 1;
-    }
-    return house_presents.len().to_string();
-}
-
-fn day3_part2(data: &str) -> String {
-    let mut house_presents: HashMap<(i32, i32), i32> = HashMap::new();
-    let mut positions = HashMap::from([("santa", (0, 0)), ("robot", (0, 0))]);
-    house_presents.insert((0, 0), 2);
-    let mut current_turn = "santa";
-    for ch in data.chars() {
-        let position = positions.get_mut(current_turn).unwrap();
-        match ch {
-            '<' => position.0 -= 1,
-            '>' => position.0 += 1,
-            '^' => position.1 += 1,
-            'v' => position.1 -= 1,
-            '\n' => {},
-            _ => {dbg!(ch); panic!("wrong input")},
-        }
-        *house_presents.entry(*position).or_insert(-1) += 1;
-        if current_turn == "santa" {
-            current_turn = "robot";
-        } else {
-            current_turn = "santa";
-        }
-    }
-    return house_presents.len().to_string();
+    Ok(input
+        .lines()
+        .filter_map(|line| line.split_whitespace().nth(0))
+        .filter_map(|number| number.parse::<i32>().ok())
+        .map(|num| num * counter.get(&num).unwrap_or(&0))
+        .sum())
 }
